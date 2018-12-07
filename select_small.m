08633e065855a7e86722ca64146615fcdde9eaab
Xiaofan Cui <richardcuixf@gmail.com>
2018-12-07 14:31:24 -0500

    Create select_small.m

---
+++ select_small.m
@@ -0,0 +1,22 @@
+function smaller = select_small(a1,a2)
+% a1 and a2 are 2 arrays of states
+N1 = length(a1);
+N2 = length(a2);
+
+if (N1<N2)
+    smaller = a1;
+elseif (N1>N2)
+    smaller = a2;
+else
+    for (i = 1:1:N1)
+        if a1(i)<a2(i)
+            smaller = a1;
+            break;
+        elseif a1(i)>a2(i)
+            smaller = a2;
+            break;
+        else
+        end
+    end
+end
+    
